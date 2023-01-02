0fb2cc56f06cbf2b410ff461ace6329fe79feff1
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-01-02 09:53:56 +0530

    commit

--- my_app_/lib/main.dart
+++ my_app_/lib/main.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      // ignore: prefer_const_constructors
+      home: Scaffold(
+        // ignore: prefer_const_constructors
+        body: Center(
+          child: const Text("thisis"),
+        ),
+      ),
+    );
+  }
+}
